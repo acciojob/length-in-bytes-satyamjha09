4bf77ca89dcc758dd312aff300d6c78026a523da
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-12-01 21:05:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
-  // write your code here
-};
-
-// Do not change the code below
+	
+  const encoder = new TextEncoder();
+  const encoded = encoder.encode(str);
+  return encoded.length
+};    
+ 
+//Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
